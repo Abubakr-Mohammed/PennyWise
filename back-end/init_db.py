e31c8c7d4453f81b4b881dfeee650a9d28f09ae4
AbubakrMohammed <mabubakr601@gmail.com>
2025-07-10 16:25:43 -0400

    Update init_db.py

--- back-end/init_db.py
+++ back-end/init_db.py
@@ -2,8 +2,8 @@ from app import app, db  # Make sure you're importing app here too!
 from models.user_model import User
 from models.transaction_model import Transaction
 
-print("📍 Starting DB initialization")
+print("Starting DB initialization")
 
 with app.app_context():
     db.create_all()
-    print("✅ Database created successfully!")
+    print("Database created successfully!")
