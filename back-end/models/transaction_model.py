80ff82e50b5b5874d77da43d5923cfb48aee595a
Daniel Yonkeu-Cheunko <daniel.yonkeucheunko@gmail.com>
2025-07-11 18:45:43 -0400

    feat: add transaction api route (create, read, delete)

--- back-end/models/transaction_model.py
+++ back-end/models/transaction_model.py
@@ -5,7 +5,7 @@ This file will contain SQLAlchemy database models
 for representing tables like Transaction, User, etc.
 Start by defining a Transaction model in Week 2.
 """
-from app import db
+from extensions import db
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +14,13 @@ class Transaction(db.Model):
     type = db.Column(db.String(10), nullable=False)  # income or expense
     date = db.Column(db.String(10), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "amount": self.amount,
+            "type": self.type,
+            "date": self.date,
+            "user_id": self.user_id
+        }
