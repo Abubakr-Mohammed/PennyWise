33525609f2cc7929803e971dc349b50cf5ba92b5
Ali Asghar <aliasgharab53@gmail.com>
2025-07-11 15:21:18 -0400

    Fixed typos and add auth route

--- back-end/app.py
+++ back-end/app.py
@@ -1,16 +1,14 @@
 from flask import Flask
-from flask_sqlachemy import SQLAlchemy
+from extensions import db
 
 app = Flask(__name__)
-
-#Setting up SQLite Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://pennywise.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pennywise.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# Import and register blueprints as usual Task B and D's job
-# app.register_blueprint(...)
+from routes.auth_routes import auth_bp
+app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
