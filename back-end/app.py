2126d4c81cd6e5e374800681d1725b700cad5956
AbubakrMohammed <mabubakr601@gmail.com>
2025-07-26 21:43:18 -0400

    Update app.py for Task C

--- back-end/app.py
+++ back-end/app.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from extensions import db
+from extensions import db, bcrypt
+from routes.transaction_routes import transaction_bp
+from routes.auth_routes import auth_bp
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pennywise.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db.init_app(app) 
+bcrypt.init_app(app)
 
-from routes.auth_routes import auth_bp
-from routes.transaction_routes import transaction_bp
-app.register_blueprint(auth_bp)
+# Register blueprints with prefix /'api'
 app.register_blueprint(transaction_bp, url_prefix='/api')
+app.register_blueprint(auth_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
