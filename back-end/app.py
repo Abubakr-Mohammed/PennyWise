04f774933c730500f89802c964a7152fe3c71bad
Daniel Yonkeu-Cheunko <daniel.yonkeucheunko@gmail.com>
2025-07-29 12:18:24 -0400

    Enabled CORS

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from extensions import db, bcrypt
 from routes.transaction_routes import transaction_bp
 from routes.auth_routes import auth_bp
 
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pennywise.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
