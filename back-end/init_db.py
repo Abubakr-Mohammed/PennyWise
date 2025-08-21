b50967e130da040d129ba3a0761cabc5fb1fa849
Daniel Yonkeu-Cheunko <daniel.yonkeucheunko@gmail.com>
2025-08-21 10:46:52 -0400

    create tables in app.py

--- back-end/init_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from models.user_model import User
-from models.transaction_model import Transaction
-
-print("Starting DB initialization")
-
-with app.app_context():
-    db.create_all()
-    print("Database created successfully!")
